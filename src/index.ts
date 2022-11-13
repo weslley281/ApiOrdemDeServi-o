956019ede1f2a5bb182c236712a787d21e045879
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-11-13 17:03:39 -0400

    the method post of users was fineshed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { establishConnection } from './database/db';
 import { usersRoutes } from './routes/users.routes';
+import swaggerUi from 'swagger-ui-express';
 
+import swaggerFile from './swagger.json';
 establishConnection();
 
 const app = express();
@@ -9,5 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/users', usersRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export { app };
