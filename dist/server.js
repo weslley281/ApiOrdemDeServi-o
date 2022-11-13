908b85c5c845df00f4438710282fd1c5e306d1fc
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-11-13 17:47:39 -0400

    A

--- dist/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require(".");
-const port = 5000;
-_1.app.listen(port, () => console.log(`The applicatition is running on http://localhost:${port}`));
