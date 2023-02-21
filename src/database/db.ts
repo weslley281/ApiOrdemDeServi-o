9bd1769ce24d859ac5abee14c268f93d3edc7591
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-02-21 15:18:39 -0400

    a

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,14 +1,34 @@
 import { Sequelize } from 'sequelize';
 
 const connection = new Sequelize('orderofservice', 'root', 'Wesv@g28', {
-  host: 'localhost',
-  dialect: 'mysql',
+  host: 'orderofservice.mysql.database.azure.com',
+  dialect: 'mssql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+  dialectOptions: {
+    encrypt: true,
+  },
 });
 
-function createConnectionDataBase(connection: Sequelize) {
-  return connection.authenticate().then(() => {
-    console.log('*******Connection made to the database!******* ');
-  });
+// const connection = mysql.createConnection({
+//   host: 'orderofservice.mysql.database.azure.com',
+//   user: 'orderofservice',
+//   password: '{your_password}',
+//   database: '{your_database}',
+//   port: 3306,
+//   ssl: { ca: fs.readFileSync('') },
+// });
+
+async function createConnectionDataBase() {
+  try {
+    await connection.authenticate();
+    console.log('*******Connection made to the database!*******');
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+  }
 }
 
 export { connection, createConnectionDataBase };
