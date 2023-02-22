7973e0a4f16a891d327db02e6ab0f6c8b329f5ed
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-02-22 10:31:03 -0300

    a

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,22 +1,27 @@
 import { Sequelize } from 'sequelize';
 
-const connection = new Sequelize('orderofservice', 'root', 'Wesv@g28', {
-  host: 'orderofservice.mysql.database.azure.com',
-  dialect: 'mssql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-  dialectOptions: {
-    encrypt: true,
-    ssl: {
-      ca: require('fs').readFileSync(
-        '../../utils/DigiCertGlobalRootCA.crt.pem'
-      ),
-    },
-  },
-});
+const connection = new Sequelize(
+  'orderofservice',
+  'orderofservice',
+  'Wesv@g28',
+  {
+    host: 'orderofservice.mysql.uhserver.com',
+    dialect: 'mysql',
+    // pool: {
+    //   max: 5,
+    //   min: 0,
+    //   idle: 10000,
+    // },
+    // dialectOptions: {
+    //   encrypt: true,
+    //   ssl: {
+    //     ca: require('fs').readFileSync(
+    //       '../../utils/DigiCertGlobalRootCA.crt.pem'
+    //     ),
+    //   },
+    // },
+  }
+);
 
 // const connection = mysql.createConnection({
 //   host: 'orderofservice.mysql.database.azure.com',
